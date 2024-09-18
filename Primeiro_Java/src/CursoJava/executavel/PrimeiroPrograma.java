d737d501e20082b53c18abfafefd36bbd8edac15
ejmax <ejmax@DESKTOP-RJNBCS4>
2024-09-18 15:46:58 -0300

    Primeira alteracao

--- Primeiro_Java/src/CursoJava/executavel/PrimeiroPrograma.java
+++ Primeiro_Java/src/CursoJava/executavel/PrimeiroPrograma.java
@@ -1,7 +1,9 @@
 package CursoJava.executavel;
-
+/* Primeiro Programa */
 public class PrimeiroPrograma {
    public static void main(String[] args) {
+	   
 	System.out.println("Meu Primeiro Programa Java");
-}
+  }
+   
 }
