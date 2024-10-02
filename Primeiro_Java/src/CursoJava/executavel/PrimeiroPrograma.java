19d834d89c455bb9da170ec2051af3547462c4b7
ejmax <ejmax@DESKTOP-RJNBCS4>
2024-10-02 19:41:58 -0300

    Atualizando

--- Primeiro_Java/src/CursoJava/executavel/PrimeiroPrograma.java
+++ Primeiro_Java/src/CursoJava/executavel/PrimeiroPrograma.java
@@ -1,9 +1,10 @@
 package CursoJava.executavel;
+
 /* Primeiro Programa */
 public class PrimeiroPrograma {
-   public static void main(String[] args) {
-	   
-	System.out.println("Meu Primeiro Programa Java");
-  }
-   
+	public static void main(String[] args) {
+
+		System.out.println("Meu Primeiro Programa Java");
+	}
+
 }
